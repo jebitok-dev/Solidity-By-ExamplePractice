f229f74804c3b1badb5e29dc6d56e6fdd029d48b
Sharon Jebitok <sharonkosgei4@gmail.com>
2022-03-28 04:44:25 +0300

    Add Data Types

---
+++ README.md
@@ -0,0 +1,41 @@
+// Solidity By Example 
+ Following the examples on soliditybyexample.org to understand solidity basic concepts better.
+
+ 
+## Built With
+
+- Solidity
+
+<!-- ## Live Demo -->
+
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+- Fork the repository
+- Git clone https://github.com/your-username/Solidity-By-ExamplePractice
+- git checkout -b branch name
+- git remote add upstream https://github.com/jebitok-dev/Solidity-By-ExamplePractice
+- git pull upstream master
+- git commit -m "commit message"
+- git push -u origin HEAD
+
+## Author
+
+👤 **Author**
+
+- GitHub: [@Jebitok-dev](https://github.com/Jebitok-dev)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgment
+
+ I hereby acknowledge the creators of [SolidityByExample](https://solidity-by-example.org).
--- data.sol
+++ data.sol
@@ -1,1 +1,40 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+// data types: boolean, uint, int, address, bytes
+contract Primitives {
+    bool public boo = true;
+
+    // uint: unsigned integer: non-negative integers of different sizes
+    // uint8 ranges from 0 to 2**8 -1
+    // uint8 ranges from 0 to 2 ** 16 - 1
+    // uint256 ranges from 0 to 2 ** 256 - 1
+    uint8 public u8 = 1;
+    uint public u256 = 456;
+    uint public u = 123; // uint stands for uint256
+
+    // int allows negative integersof different ranges ranging from int8 to int256
+    // int256 ranges from -2 ** 255 to 2 ** 255 - 1
+    // int128 ranges from -2 ** 127 to 2 ** 127 - 1
+    int8 public i8 = -1;
+    int public i256 = 456;
+    int public i = -123; // int stands for int256
+
+    // min & max int
+    int public minInt = type(int).min;
+    int public maxInt = type(int).max;
+
+    address public addr = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
+
+    // bytes represent sequence of bytes
+    // 2 types of bytes types: fixed-size & dynamically-sized byte arrays
+    // solidity uses dynamically-sized array of bytes byte[](shorthand)
+
+    bytes1 a = 0xb5; [10110101]
+    bytes1 b = 0x56; [01010110]
+
+    // default values: unassigned variables have default value
+    bool public defaultBoo; // false
+    uint public defaultUint; // 0
+    int public defaultInt; // 0
+    address public defaultAddr; // 0x0000000000000000000000000000000000000000
